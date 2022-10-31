38180347f51cd3db0c832cdfbbfcda280f9fc5d4
ogabek16 <117097664+ogabek16@users.noreply.github.com>
2022-10-31 20:08:20 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+a = input("Enter number: ") # Foydaloanuvchidan ma`lumot olyapti
+
+if a[-1::-1] != a: # Agar a ni teskarisi o'ziga teng bo'lsa
+    print("Is not palindrome") # Natija
+else: # Aks holda
+    print("Is palindrome") # Natija
+    
+    
